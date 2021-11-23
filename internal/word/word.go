7e8dbb345ee0d8ea04a254061db8756c242da6f2
dingdong <dingdong@miao.com>
2021-11-23 16:20:24 +0800

    add .

--- internal/word/word.go
+++ internal/word/word.go
@@ -36,12 +36,13 @@ func CamelCaseToUnderscore(s string) string {
 	for i, r := range s {
 		if i == 0 {
 			output = append(output, unicode.ToLower(r))
-		} else {
-			if unicode.IsUpper(r) {
-				output = append(output, '_')
-			}
-			output = append(output, unicode.ToLower(r))
+			continue
 		}
+
+		if unicode.IsUpper(r) {
+			output = append(output, '_')
+		}
+		output = append(output, unicode.ToLower(r))
 	}
 
 	return string(output)
