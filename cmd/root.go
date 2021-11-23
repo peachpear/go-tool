b1b948aefb295f7d18e2935c869755f1a8a86fbc
dingdong <dingdong@miao.com>
2021-11-23 16:19:11 +0800

    add .

--- cmd/root.go
+++ cmd/root.go
@@ -4,10 +4,10 @@ import "github.com/spf13/cobra"
 
 var rootCmd = &cobra.Command{}
 
-func Execute() error {
-	return rootCmd.Execute()
-}
-
 func init() {
 	rootCmd.AddCommand(wordCmd)
+}
+
+func Execute() error {
+	return rootCmd.Execute()
 }
--- internal/word/word.go
+++ internal/word/word.go
@@ -17,15 +17,32 @@ func ToLower(s string) string {
 
 // 下划线转大写驼峰
 func UnderscoreToUpperCamelCase(s string) string {
-	s = strings.Replace(s, "_", "", -1)
+	s = strings.Replace(s, "_", " ", -1)
 	s = strings.Title(s)
 
 	return strings.Replace(s, " ", "", -1)
 }
 
 // 下线线转小写驼峰
-func UnderscoreToUpperLowerCase(s string) string {
+func UnderscoreToLowerCamelCase(s string) string {
 	s = UnderscoreToUpperCamelCase(s)
 
 	return string(unicode.ToLower(rune(s[0]))) + s[1:]
+}
+
+// 驼峰转下划线
+func CamelCaseToUnderscore(s string) string {
+	var output []rune
+	for i, r := range s {
+		if i == 0 {
+			output = append(output, unicode.ToLower(r))
+		} else {
+			if unicode.IsUpper(r) {
+				output = append(output, '_')
+			}
+			output = append(output, unicode.ToLower(r))
+		}
+	}
+
+	return string(output)
 }
